32e2ad5ae29058d76dc0199c04da75bac2354997
Rousseau Lobo Braga <rbraga@tjro.jus.br>
2018-08-08 12:58:36 -0400

    Primeiro envio

---
+++ src/app/sidebar/sidebar.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+import { Router, NavigationEnd } from '@angular/router';
+
+@Component({
+  selector: 'app-sidebar',
+  templateUrl: './sidebar.component.html',
+  styleUrls: ['./sidebar.component.scss']
+})
+export class SidebarComponent implements OnInit {
+
+  public currentUrl: string;
+
+  constructor(private router: Router) {
+    router.events.subscribe((_: NavigationEnd) => { 
+      if (_.url != null) {
+
+        this.currentUrl = _.url;
+      }
+     });
+   }
+
+  ngOnInit() {
+  }
+
+}
